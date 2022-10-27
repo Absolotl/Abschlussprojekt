395ab397f3f20cf529ed65ba497e679ceea27292
Absolotl <92368485+Absolotl@users.noreply.github.com>
2022-10-27 16:25:09 +0200

    Add files via upload

--- app.py
+++ app.py
@@ -1,6 +1,8 @@
 from asyncio.windows_events import NULL
 from flask import Flask, render_template, url_for, request, redirect
 from flask_sqlalchemy import SQLAlchemy
+from flask import send_file
+from flask import send_from_directory
 from datetime import date, datetime
 import numpy as np
 import pandas as pd
@@ -13,15 +15,11 @@ import datetime
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///instance/menu.db'
 db = SQLAlchemy(app)
-
 conn = sqlite3.connect('instance/menu.db')
 c = conn.cursor()
-c.execute("create table if not exists Choice(id INTEGER primary key, nr Integer, name varchar(40), ingridient varchar(80), prize REAL)")
+c.execute("create table if not exists Choice(id INTEGER primary key autoincrement, nr Integer, name varchar(40), ingridient varchar(80), prize REAL)")
 selection = pd.read_sql_query("Select * from MENU", conn)
-#selection = pd.read_sql_query("Select * from choice", conn)
-#selection = pd.read_sql_query("select sum(prize) from choice", conn)
-row_data=list(selection.values.tolist())
-localID = 0
+selection2 = pd.read_sql_query("Select * from choice", conn)
 
 class Choice(db.Model):
     id = db.Column(db.Integer, primary_key=True)
@@ -32,62 +30,56 @@ class Choice(db.Model):
     def __repr__(self):
         return '<Task %r>' % self.id
 
-new_task = Choice()
-
 @app.route('/', methods=("POST", "GET"))
 def Html_table():
+    conn = sqlite3.connect('instance/menu.db')
+    c = conn.cursor()
+    selection2 = pd.read_sql_query("Select * from choice", conn)
+    c.execute("select sum(prize) from choice")
+    for row in c.fetchall():
+        summe = row[0]
+        if summe == None:            
+            summe = ''
     return render_template("index.html", column_names=['Name', 'Bezeichnung', 'Preis', 'Aktion'],
-                            row_data=list(selection.values.tolist()), zip=zip)
+                            row_data=list(selection.values.tolist()),
+                            row_data2=list(selection2.values.tolist()),
+                            row_data3=summe, zip=zip)
 
 #add
 @app.route('/add/<int:id>', methods=['POST', 'GET'])
 def add(id):
     print(request.method)
     if request.method == 'GET':
-        global localID
         print(request.method)
         conn = sqlite3.connect('instance/menu.db')
         c = conn.cursor()
-        c.execute("create table if not exists Choice(id INTEGER primary key, nr Integer, name varchar(40), ingridient varchar(80), prize REAL)")
+        c.execute("create table if not exists Choice(id INTEGER primary key autoincrement, nr Integer, name varchar(40), ingridient varchar(80), prize REAL)")
         c.execute('select * from menu where id = ' + str(id))
         for row in c.fetchall():
-            new_task = Choice(id=localID, nr=row[0], name=row[1], ingridient=row[2], prize=row[3])
-            localID += 1
-
-        selection = pd.read_sql_query("Select * from Choice", conn)
-
+            new_task = Choice(nr=row[0], name=row[1], ingridient=row[2], prize=row[3])
         try:
             db.session.add(new_task)
             db.session.commit()
             return redirect('/')
-            #return render_template("index.html", row_data2=list(selection.values.tolist()), zip=zip)
         except:
             print("There was an issue adding your task")
             return 'There was an issue adding your task'
-    else:
-        print(request.method)
-        #tasks = Choice.query.order_by(Choice.id).all()
-        return render_template("index.html", row_data2=list(selection.values.tolist()), zip=zip)
-
 
 #add + Beilage
 @app.route('/beilage/<int:id>', methods=['POST', 'GET'])
 def Test(id):
-    global localID
     global new_task
     conn = sqlite3.connect('instance/menu.db')
     c = conn.cursor()
-    c.execute("create table if not exists Choice(id INTEGER primary key, nr Integer, name varchar(40), ingridient varchar(80), prize REAL)")
-    
+    c.execute("create table if not exists Choice(id INTEGER primary key autoincrement, nr Integer, name varchar(40), ingridient varchar(80), prize REAL)")
     if request.method == 'POST':
+        print("post")
         new_task.name = new_task.name + ', Beilage: '+ request.form['beilage']        
         print("beilage gewählt")
-        selection = pd.read_sql_query("Select * from Choice", conn)
-        try:
+        try:            
             db.session.add(new_task)
             db.session.commit()
             return redirect('/')
-            #return render_template("index.html", row_data2=list(selection.values.tolist()), zip=zip)
         except:
             print("There was an issue adding your task (Beilage)")
             return 'There was an issue adding your task'
@@ -95,17 +87,14 @@ def Test(id):
         c.execute('select * from menu where id = ' + str(id))
         print('id: '+ str(id))
         for row in c.fetchall():
-            new_task = Choice(id=localID, nr=row[0], name=row[1], ingridient=row[2], prize=row[3])
-            localID += 1        
+            new_task = Choice(nr=row[0], name=row[1], ingridient=row[2], prize=row[3])     
         print('beilage wählen')
         return render_template("beilage.html", task = new_task)
-
 
 #delete 
 @app.route('/delete/<int:id>')
 def Delete(id):
     task_to_delete = Choice.query.get_or_404(id)
-
     try:
         db.session.delete(task_to_delete)
         db.session.commit()
@@ -122,6 +111,7 @@ def ClearTable():
     conn = sqlite3.connect('instance/menu.db')
     c = conn.cursor()
     c.execute('drop table choice')
+    c.execute("create table if not exists Choice(id INTEGER primary key autoincrement, nr Integer, name varchar(40), ingridient varchar(80), prize REAL)")
     return redirect('/')
 
 #PDF
@@ -132,6 +122,9 @@ def CreatePdf():
     y = 640
 
     pdf = canvas.Canvas('Bestellung_' + str(datetime.date.today()) + '.pdf')
+    filename = 'Bestellung_' + str(datetime.date.today()) + '.pdf'
+    path = "C:\\Users\\Empfang\\Documents\\Eigene Dokumente\\Abschlussprojekt\\"
+    print(path + filename)
     pdf.setTitle('Bestellung')
     pdf.setFont("Courier", 12)
     pdf.drawString(30, 800, 'PRIOKOM Informationstechnik GmbH')
@@ -151,9 +144,10 @@ def CreatePdf():
     pdf.setFont("Courier", 12)
     pdf.drawString(30, y-20, 'Mit freundlichen Grüßen')
     pdf.drawString(30, y-40, 'PRIOKOM Informationstechnik GmbH')
-    pdf.drawString(30, 500, 'fitzli putzli')
     pdf.save()
-    return NULL
+
+    return send_from_directory(path, filename, as_attachment=True)
+    #return send_file(path, as_attachment=True)
 
 if __name__== "__main__":
     app.run(debug=True)
